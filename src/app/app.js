770e8e7004599b90bc4d59b03023311b46db71d0
Jan König <jankoenig120190@googlemail.com>
2018-11-19 18:42:01 +0100

    🎉 Add v2 sample voice app

---
+++ src/app/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const {App} = require('jovo-framework');
+const {Alexa} = require('jovo-platform-alexa');
+const {GoogleAssistant} = require('jovo-platform-googleassistant');
+const {JovoDebugger} = require('jovo-plugin-debugger');
+
+const app = new App();
+
+app.use(
+    new Alexa(),
+    new GoogleAssistant(),
+    new JovoDebugger()
+);
+
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+    LAUNCH() {
+        this.toIntent('HelloWorldIntent');
+    },
+
+    HelloWorldIntent() {
+        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    },
+
+    MyNameIsIntent() {
+        this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+    },
+});
+
+module.exports.app = app;
